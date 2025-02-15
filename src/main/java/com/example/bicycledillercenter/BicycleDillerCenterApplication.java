5e1da32d5f35bb5f07e5f853dbbeaacbe3267bc7
Mikhail Karduban <michaelmaksimovich7@gmail.com>
2025-02-15 12:21:23 +0300

    initial commit

---
+++ src/main/java/com/example/bicycledillercenter/BicycleDillerCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.bicycledillercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleDillerCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BicycleDillerCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bicycledillercenter/BicycleDillerCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bicycledillercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleDillerCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
