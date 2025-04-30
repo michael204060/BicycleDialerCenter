39fe826d469ebaef5fd936354c6e6a5f131a3a89
michael204060 <michaelmaksimiovich7@gmail.com>
2025-04-30 14:12:24 +0300

    Created REST API with get, put, delete endpoints, some unimportant changes.

--- src/main/java/com/bikedc/controller/StatsController.java
+++ src/main/java/com/bikedc/controller/StatsController.java
@@ -1,6 +1,7 @@
 package com.bikedc.controller;
 
 import com.bikedc.service.VisitCounterService;
+import org.hibernate.annotations.Synchronize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/bikedc/service/impl/VisitCounterServiceImpl.java
+++ src/main/java/com/bikedc/service/impl/VisitCounterServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bikedc.service.impl;
 
 import com.bikedc.service.VisitCounterService;
+import lombok.Synchronized;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -10,19 +11,22 @@ import java.util.stream.Collectors;
 
 @Service
 public class VisitCounterServiceImpl implements VisitCounterService {
-    private final ConcurrentHashMap<String, AtomicLong> counters = new ConcurrentHashMap<>();
+    private final Map<String, AtomicLong> counters = new ConcurrentHashMap<>();
 
     @Override
+    @Synchronized 
     public void incrementCounter(String endpoint) {
         counters.computeIfAbsent(endpoint, k -> new AtomicLong(0)).incrementAndGet();
     }
 
     @Override
+    @Synchronized
     public long getCount(String endpoint) {
         return counters.getOrDefault(endpoint, new AtomicLong(0)).get();
     }
 
     @Override
+    @Synchronized
     public Map<String, Long> getVisitStats() {
         return counters.entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().get()));
